3259fe97bc3b0fba5335cc820c50d13cd9b5f464
lamchristine <christine.lam.06@gmail.com>
2016-04-17 23:34:59 -0700

    crud working

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -1,7 +1,34 @@
 angular.module('libraryApp')
   .controller('BooksIndexController', BooksIndexController);
 
+var endpoint = "https://super-crud.herokuapp.com/books";
+
 BooksIndexController.$inject=['$http'];
 function BooksIndexController( $http ) {
   var vm = this;
+
+  //show all books
+  $http({
+    method: 'GET',
+    url: endpoint,
+  }).then(function successCallBack (response) {
+    vm.books = response.data.books;
+  }, function errorCallBack (response){
+    console.log('Error in getting data', response);
+  });
+
+  //create new book
+  vm.createBook = function () {
+      $http({
+        method: 'POST',
+        url: endpoint,
+        data: vm.newBook,
+      }).then(function successAdd(response){
+        vm.books.push(response.data);
+      }, function errorAdd(response) {
+        console.log('There was an error in posting', response);
+      });
+  };
+
+
 }
--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -1,7 +1,55 @@
 angular.module('libraryApp')
-  .controller('BooksIndexController', BooksIndexController);
+  .controller('BooksShowController', BooksShowController);
+
+  var endpoint = 'https://super-crud.herokuapp.com/books';
+
 
 BooksShowController.$inject=['$http', '$routeParams', '$location'];
 function BooksShowController($http, $routeParams, $location) {
   var vm = this;
-};
+
+  //show one book
+  $http({
+    method: 'GET',
+    url: endpoint + '/' + $routeParams.id
+  }).then(function successCallBack(json) {
+    // console.log(json);
+    vm.book = json.data;
+  }, function errorCallBack(json){
+    console.log("ERROR!");
+  });
+
+
+  //edit a book
+  vm.editBook = function (bookUpdate) {
+    console.log("edit",  bookUpdate)
+    $http({
+      method: 'PUT',
+      url: endpoint + '/' + bookUpdate._id,
+      data: {
+        title: bookUpdate.title,
+        author: bookUpdate.author,
+        image: bookUpdate.image,
+        releaseDate: bookUpdate.releaseDate
+      }
+    }).then(function successCallBack(response) {
+      vm.book = response.data;
+      $location.path('/');
+    }, function errorCallBack(response) {
+      console.log('Error in editing data', response);
+    });
+  };
+
+  //delete a book
+  vm.deleteBook = function (book) {
+    console.log("deleting", book);
+    $http({
+      method: 'DELETE',
+      url: endpoint + ('/') + book._id
+    }).then(function successCallBack(response) {
+      console.log("deleted", response);
+      $location.path('/');
+    });
+  };
+
+}
